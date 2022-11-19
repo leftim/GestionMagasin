1551b7f17c33a10b4f0b24d81527a9d49aff3617
Your Name <you@example.com>
2022-11-19 20:25:19 +0100

    service avancés

---
+++ src/main/java/com/example/gestionmagasin/controllers/FactureRestController.java
@@ -0,0 +1,2 @@
+package com.example.gestionmagasin.controllers;public class FactureRestController {
+}
---
+++ src/main/java/com/example/gestionmagasin/repositories/FournisseurRepository.java
@@ -0,0 +1,2 @@
+package com.example.gestionmagasin.repositories;public interface FournisseurRepository {
+}
